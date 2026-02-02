9eeb541c16c3c49a03bee1ce1dfd9fff38ac9fac
Niko-Cloud <indraniko20@gmail.com>
2026-02-02 23:09:59 +0700

    feat(db) : add migration for my own base information (nothing vital don't worry)

--- migration/0016_seed_showcase.up.sql
+++ migration/0016_seed_showcase.up.sql
@@ -2,8 +2,8 @@ INSERT INTO showcase
 (title, summary, stack, repo_url, live_url)
 VALUES('In My Work Repo', 'Cannot Be Shared', '{"Java, Mysql, Flutter"}', 'SECRET', '');
 INSERT INTO showcase
-(id, title, summary, stack, repo_url, live_url, created_at, updated_at)
+(id, title, summary, stack, repo_url, live_url)
 VALUES('Cli Portofolio', 'Basically This Web (Portofolio Terminal Style)', '{Golang,Flutter,Postgre}', 'tba', '');
 INSERT INTO showcase
-(id, title, summary, stack, repo_url, live_url, created_at, updated_at)
+(id, title, summary, stack, repo_url, live_url)
 VALUES('Absensi Glagahwangi', 'My Thesis Flutter App', '{Flutter,Firebase,React}', 'https://github.com/Niko-Cloud/Absensi-Glagahwangi-Android', '');
